b435a2affa5f1dd68695a803d6a3535dfa6fe272
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-01-23 01:03:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.scss';
+import {BrowserRouter, Route} from "react-router-dom";
+import AddTodos from "./component/add-todos.component";
+import LoginPage from "./component/login.page";
+import RegisterPage from "./component/register.page";
+
+
+const App = () => {
+
+
+    return (
+        <BrowserRouter>
+            <Route exact path='/todos'>
+                <AddTodos/>
+            </Route>
+            <Route exact path='/login'>
+                <LoginPage></LoginPage>
+            </Route>
+            <Route exact path='/register'>
+                <RegisterPage></RegisterPage>
+            </Route>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/component/todo-item.component.tsx
@@ -0,0 +1,48 @@
+import {useState} from "react"
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import './todo-item.component.scss';
+import CheckIcon from '@mui/icons-material/Check';
+
+const TodoItem = ({item, onDelete, onEdit}: any) => {
+
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [editedTodoName, setEditedTodoName] = useState('');
+
+    const changeTodoName = (e: any) => {
+        setEditedTodoName(e.target.value)
+    }
+
+
+    const enableEditMode = () => {
+        setIsEditMode(true)
+    }
+
+    const disableEditMode = () => {
+        setIsEditMode(false)
+        onEdit(editedTodoName)
+    }
+
+
+    return (
+        <div className='todo-item'>
+            {
+                isEditMode ?
+                    <input value={editedTodoName} onChange={changeTodoName}/>
+                    : <div>{item}</div>
+
+            }
+            <div>
+                {
+                    isEditMode ?
+                        <CheckIcon onClick={disableEditMode}/>
+                        : <EditIcon onClick={enableEditMode}/>
+
+                }
+                <DeleteIcon onClick={onDelete}/>
+            </div>
+        </div>
+    );
+}
+
+export default TodoItem;
